79c1470f543ed119d109aa016e243f50eefbeb89
alumno <alumno@vm1>
2021-12-13 09:55:49 +0100

    Final del examen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Municipio;
+use App\Models\Localidad;
+use App\Models\Terremoto;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,18 +19,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Model::unguard();
+        self::seedMunicipios();
+        self::seedLocalidades();
+        self::seedTerremotos();
         Schema::disableForeignKeyConstraints();
 
-        $this->call(MunicipiosTableSeeder::class);
-
-        $this->call(LocalidadesTableSeeder::class);
-
-        $this->call(TerremotosTableSeeder::class);
-
-        Model::reguard();
-
         Schema::enableForeignKeyConstraints();
+    }
+
+    private static function seedMunicipios()
+    {
+        Municipio::truncate();
+        foreach (self::$municipios as $municipio) {
+            $m = new Municipio;
+            $m->nombre = $municipio['nombre'];
+            $m->poblacion = $municipio['poblacion'];
+            $m->save();
+        }
+    }
+
+    private static function seedLocalidades()
+    {
+        Localidad::truncate();
+        foreach (self::$localidades as $localidad) {
+            $l = new Localidad;
+            $l->nombre = $localidad['nombre'];
+            $l->estado = $localidad['estado'];
+            $l->poblacion = $localidad['poblacion'];
+            $l->municipio_id = $localidad['municipio_id'];
+            $m->save();
+        }
+    }
+
+    private static function seedTerremotos()
+    {
+        Terremoto::truncate();
+        foreach(self::$terremotos as $terremoto) {
+            if($terremoto['Magnitud'] >= 4) {
+                $t = new Terremoto;
+                $t->OBJECTID = $terremoto['OBJECTID'];
+                $t->DateTime = $terremoto['DateTime'];
+                $t->ErrTime = $terremoto['ErrTime'];
+                $t->RMS = $terremoto['RMS'];
+                $t->Latitude = $terremoto['Latitude'];
+                $t->Longitude = $terremoto['Longitude'];
+                $t->Depth = $terremoto['Depth'];
+                $t->Magnitud = $terremoto['Magnitud'];
+                $t->save();
+            }
+        }
     }
 }
